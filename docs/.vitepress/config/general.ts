afd3f98ac2e8dbf5f2260e001f50ec1e37b1e95a
哈哈先生 <598652647@qq.com>
2025-12-09 22:27:47 +0800

    fix(docs): 启用F12开发者模式禁用功能

--- docs/.vitepress/config/general.ts
+++ docs/.vitepress/config/general.ts
@@ -105,7 +105,7 @@ export const general = defineConfig({
     plugins: [
       groupIconVitePlugin(),
       vitepressProtectPlugin({
-        disableF12: false, // 禁用F12开发者模式
+        disableF12: true, // 禁用F12开发者模式
         disableCopy: false, // 禁用文本复制
         disableSelect: false // 禁用文本选择
       })
